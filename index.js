37f8fe19830649d6c1ee745d341afa13de02c697
wangkodok <wangkodok@gmail.com>
2023-11-20 19:52:29 +0900

    ejs 파일에 값을 전달하고 작업

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const path = require("path");
 app.set("views", path.join(__dirname, "/"));
 app.set("view engine", "ejs");
 
+const data = [
+  {
+    language: "JavaScript",
+    lv: null,
+  },
+  {
+    language: "Node",
+    lv: null,
+  },
+  {
+    language: "React",
+    lv: null,
+  },
+  {
+    language: "TypeScript",
+    lv: null,
+  },
+];
+
 app.get("/", (req, res) => {
-  res.render("start");
+  res.render("start", { data });
 });
 
 app.get("/intro", (req, res) => {
